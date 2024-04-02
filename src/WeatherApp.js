aff32e1d18fcddfa2a37bf41bbc2a47158c56227
CarlosP <carlos.pettersen@gbst.com>
2024-04-02 15:18:50 +1100

    added more styling and functionality

--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Button, Box, Container, Input, Typography } from "@mui/material";
+import {
+  Button,
+  Box,
+  Container,
+  Input,
+  Typography,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 const Weather = () => {
   const [weatherData, setWeatherData] = useState(null);
   const [inputCity, setInputCity] = useState("");
-  // const API_KEY = "b0ecf3bcc25e5810d8c7b4e68bac182c";
-  // const units = "metric";
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleInputChange = (e) => {
     setInputCity(e.target.value);
   };
 
   const fetchWeather = () => {
-    // First, call the geocoding API to get latitude and longitude
+    setLoading(true);
+    setError(null);
     axios
       .get(
         `https://geocoding-api.open-meteo.com/v1/search?name=${inputCity}&count=1&language=en&format=json`,
@@ -21,7 +35,6 @@ const Weather = () => {
       .then((response) => {
         const { latitude, longitude } = response.data.results[0];
 
-        // Then, call both weather forecast APIs simultaneously
         return axios.all([
           axios.get(
             `https://marine-api.open-meteo.com/v1/marine?latitude=${latitude}&longitude=${longitude}&hourly=wave_height,wave_direction,wave_period&timezone=Australia%2FSydney`,
@@ -33,87 +46,150 @@ const Weather = () => {
       })
       .then(
         axios.spread((marineResponse, forecastResponse) => {
-          // Handle both responses here
           const marineData = marineResponse.data;
           const forecastData = forecastResponse.data;
 
-          // Combine or process the data as needed
-
-          // Update state with the combined data or whichever data you need
           setWeatherData({ marineData, forecastData });
-          console.log(marineData, forecastData);
+          setLoading(false);
         }),
       )
       .catch((error) => {
         console.error("Error fetching weather data:", error);
-        setWeatherData(null);
+        setError("Error fetching weather data. Please try again.");
+        setLoading(false);
       });
   };
 
   const getDirectionArrow = (degree) => {
-    // Convert degree to compass direction
     const compassDirections = ["N", "NE", "E", "SE", "S", "SW", "W", "NW", "N"];
     const index = Math.round(degree / 45) % 8;
     const direction = compassDirections[index];
+    const degreeValue = degree.toFixed(0); // Round the degree value to remove decimal points
 
-    // Map compass direction to arrow character
     const arrowMap = {
-      N: "↓",
-      NE: "↙",
-      E: "←",
-      SE: "↖",
-      S: "↑",
-      SW: "↗",
-      W: "→",
-      NW: "↘",
+      N: `↓ (${degreeValue}°)`,
+      NE: `↙ (${degreeValue}°)`,
+      E: `← (${degreeValue}°)`,
+      SE: `↖ (${degreeValue}°)`,
+      S: `↑ (${degreeValue}°)`,
+      SW: `↗ (${degreeValue}°)`,
+      W: `→ (${degreeValue}°)`,
+      NW: `↘ (${degreeValue}°)`,
     };
 
     return arrowMap[direction];
   };
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const isCurrentRow = (index) => {
+    const date = new Date(weatherData.marineData.hourly.time[index]);
+    return (
+      currentDate.getFullYear() === date.getFullYear() &&
+      currentDate.getMonth() === date.getMonth() &&
+      currentDate.getDate() === date.getDate() &&
+      currentDate.getHours() === date.getHours()
+    );
+  };
+
   return (
-    <Container>
+    <Container style={{ backgroundColor: "#f0f0f0", padding: "20px" }}>
       <Typography
         variant="h2"
-        sx={{ mt: 4, mb: 2, textAlign: "center", color: "primary.main" }}
+        align="center"
+        style={{ marginBottom: "20px", color: "navy" }}
       >
         Weather Information
       </Typography>
       <Box
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
         <Input
           type="text"
           placeholder="Enter city"
           value={inputCity}
           onChange={handleInputChange}
-          sx={{ mr: 2, width: "300px" }}
+          style={{ marginRight: "10px", width: "300px" }}
         />
-        <Button variant="contained" onClick={fetchWeather}>
-          Get Weather
+        <Button
+          variant="contained"
+          onClick={fetchWeather}
+          style={{ backgroundColor: "navy", color: "white" }}
+        >
+          {loading ? "Fetching..." : "Get Weather"}
         </Button>
       </Box>
+      {error && (
+        <Typography variant="body1" style={{ color: "red", marginTop: "10px" }}>
+          {error}
+        </Typography>
+      )}
       {weatherData && (
-        <Box sx={{ mt: 4, textAlign: "center" }}>
-          <Typography variant="h5">{inputCity}</Typography>
-          <Typography variant="body1">
-            Time | Wave Period | Wave Direction | Wave Height
+        <Box style={{ marginTop: "20px" }}>
+          <Typography variant="h5" align="center" style={{ color: "navy" }}>
+            {inputCity}
           </Typography>
-          {weatherData.marineData.hourly.time.map((time, index) => {
-            const date = new Date(time);
-            const formattedDate = `${date.getDate()}/${date.getMonth() + 1} at ${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
-            const waveDirectionDegree =
-              weatherData.marineData.hourly.wave_direction[index];
-            const directionArrow = getDirectionArrow(waveDirectionDegree);
-            return (
-              <Typography key={index} variant="body1">
-                {formattedDate} |{" "}
-                {weatherData.marineData.hourly.wave_period[index]}s |{" "}
-                {directionArrow} |{" "}
-                {weatherData.marineData.hourly.wave_height[index]}m
-              </Typography>
-            );
-          })}
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>Time</TableCell>
+                  <TableCell>Wave Direction</TableCell>
+                  <TableCell>Wave Period (s)</TableCell>
+                  <TableCell>Wave Height (m)</TableCell>
+                  <TableCell>Wind Direction</TableCell>
+                  <TableCell>Wind Speed (km/h)</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {weatherData.marineData.hourly.time.map((time, index) => {
+                  const date = new Date(time);
+                  const formattedDate = `${date.getDate()}/${date.getMonth() + 1} at ${("0" + date.getHours()).slice(-2)}:${("0" + date.getMinutes()).slice(-2)}`;
+                  const waveDirectionDegree =
+                    weatherData.marineData.hourly.wave_direction[index];
+                  const directionArrowWave =
+                    getDirectionArrow(waveDirectionDegree);
+                  const windDirectionDegree =
+                    weatherData.forecastData.hourly.wind_direction_10m[index];
+                  const directionArrowWind =
+                    getDirectionArrow(windDirectionDegree);
+                  return (
+                    <TableRow
+                      key={index}
+                      style={
+                        isCurrentRow(index)
+                          ? { backgroundColor: "rgba(0, 0, 255, 0.1)" }
+                          : {}
+                      }
+                    >
+                      <TableCell>{formattedDate}</TableCell>
+                      <TableCell>{directionArrowWave}</TableCell>
+                      <TableCell>
+                        {weatherData.marineData.hourly.wave_period[index]}
+                      </TableCell>
+                      <TableCell>
+                        {weatherData.marineData.hourly.wave_height[index]}
+                      </TableCell>
+                      <TableCell>{directionArrowWind}</TableCell>
+                      <TableCell>
+                        {weatherData.forecastData.hourly.wind_speed_10m[index]}
+                      </TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Box>
       )}
     </Container>
